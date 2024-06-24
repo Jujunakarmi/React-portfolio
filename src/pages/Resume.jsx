000b119f7d9a4908a27595a33fa412e75f153d12
Juju Nakarmi <152660757+Jujunakarmi@users.noreply.github.com>
2024-06-24 17:14:17 -0400

    added resume component

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,8 +1,33 @@
+import React from 'react';
+import resume from "../assets/resume.pdf"
+
 export default function Resume() {
-    
-    return (
-   <>
-   resume</>
-    );
-  }
-  
+  return (
+    <div>
+      <h2>Resume</h2>
+      <p>
+        Click <a href={resume} target="_blank">here</a> to view my resume.
+        <h3>Front-End Proficiencies</h3>
+          <ul className="skills">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>jQuery</li>
+            <li>Responsive Design</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+          </ul>
+        <h3>Back-End Proficiencies</h3>
+        <ul className="skills">
+            <li>APIs</li>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>MySQL, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>REST</li>
+            <li>GraphQL</li>
+          </ul>
+      </p>
+    </div>
+  );
+}
